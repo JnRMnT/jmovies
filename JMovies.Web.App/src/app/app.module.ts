9b330cb5ea30b51f15b4750b23eaabb6f9c523f7
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2019-07-26 00:20:59 +0200

    Angular Material is added

--- JMovies.Web.App/src/app/app.module.ts
+++ JMovies.Web.App/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
